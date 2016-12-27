383424ee35da58552113f260d8c00c6fd1c3c962
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-27 10:29:11 -0800

    UI for main activity

--- app/src/main/java/com/yahoo/palagummi/memorableplaces/MainActivity.java
+++ app/src/main/java/com/yahoo/palagummi/memorableplaces/MainActivity.java
@@ -2,12 +2,39 @@ package com.yahoo.palagummi.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView listView;
+    ArrayList<String> places = new ArrayList<String>();
+
+
+    public void addNewPlace(View view) {
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+            }
+        });
     }
 }
